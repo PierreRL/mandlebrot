a77f5528f18f9ea82cf3cac4e6fdb6963e62193e
Pierre Lardet <lardet.pierre@gmail.com>
2021-06-04 20:03:51 +0100

    colour updates and readme

--- mandlebrot_generator.js
+++ mandlebrot_generator.js
@@ -15,15 +15,15 @@ function drawImage() {
             var complexNX = ((x / resolutionX * myWidth + ((resolutionX / myWidth) / 2)) - centerX) / scale;
             var complexNY = ((y / resolutionY * myHeight + ((resolutionY / myHeight) / 2)) - centerY) / scale;
             var complexConstant = new CNum(complexNX, complexNY).add(offset);
-            
-            
+
+
             var complex = new CNum(0, 0);
             var stable = true;
             var i = 0;
-            
+
             do {
 
-                
+
                 complex = complex.square();
                 complex = complex.add(complexConstant);
 
@@ -33,55 +33,55 @@ function drawImage() {
                 else {
                     i++;
                 }
-                
+
             } while ((complex.magnitude() < 2 && i < iterations));
-           
-            
-          
-            var percentage = i/iterations;
+
+
+
+            var percentage = i / iterations;
             if (i == iterations) {
                 ctx.fillStyle = "#000000";
             }
             else {
-                var rgb = hslToRgb(percentage, 1, 0.5);
+                var rgb = hslToRgb(percentage, 1, 0.45);
                 ctx.fillStyle = "rgb(" + rgb[0] + "," + rgb[1] + "," + rgb[2] + ")";
             }
             //comment the above block and uncooment below for other colors
             /* else {
                 ctx.fillStyle = "rgb(" + percentage*255+ ",0,0)"
             } */
-            
-            
+
+
 
 
             ctx.fillRect(x * (myWidth / resolutionX), y * (myHeight / resolutionY), myWidth / resolutionX + 1, myHeight / resolutionY + 1);
-            
+
         }
-        text.innerHTML = Math.round((y/resolutionY)*100) + "%";
+        text.innerHTML = Math.round((y / resolutionY) * 100) + "%";
     }
 
 }
 
-function hslToRgb(h, s, l){
+function hslToRgb(h, s, l) {
     var r, g, b;
 
-    if(s == 0){
+    if (s == 0) {
         r = g = b = l; // achromatic
-    }else{
-        var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    } else {
+        var hue2rgb = function hue2rgb(p, q, t) {
+            if (t < 0) t += 1;
+            if (t > 1) t -= 1;
+            if (t < 1 / 6) return p + (q - p) * 6 * t;
+            if (t < 1 / 2) return q;
+            if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
             return p;
         }
 
         var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
         var p = 2 * l - q;
-        r = hue2rgb(p, q, h + 1/3);
+        r = hue2rgb(p, q, h + 1 / 3);
         g = hue2rgb(p, q, h);
-        b = hue2rgb(p, q, h - 1/3);
+        b = hue2rgb(p, q, h - 1 / 3);
     }
 
     return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 A simple mandlebrot set visualiser created in the javascript canvas.
 
-To run it, simply open index.html. To zoom into different parts of the set, drag a box around the location you'd like to zoom in on. Settings can be changed in the mandlebrot_generator.js file.
+To run it, simply open index.html. (You might have to wait a little second as the algorithm takes some time to finish calculating every pixel). To zoom into different parts of the set, drag a box around the location you'd like to zoom in on. Settings can be changed in the mandlebrot_generator.js file.
